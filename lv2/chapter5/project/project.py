275273da5c1bb5dd52388481fb71825591331d4d
thaycacac | phatto pham <thaycacac@gmail.com>
2019-09-07 23:56:57 +0700

    add: chap6 lv2

--- lv2/chapter5/project/project.py
+++ lv2/chapter5/project/project.py
@@ -1,24 +1,24 @@
-# import turtle
+import turtle
 
-# screen = turtle.Screen()
-# screen.setup(350, 500)
-# screen.bgpic("background.png")
-# bird =  turtle.Turtle()
-# bird.shape("triangle")
+screen = turtle.Screen()
+screen.setup(350, 500)
+screen.bgpic("background.png")
+bird =  turtle.Turtle()
+bird.shape("triangle")
 
-# move_speed = 10
-# turn_speed = 10
+move_speed = 10
+turn_speed = 10
 
-# def forward(): bird.forward(move_speed)
-# def backward(): bird.backward(move_speed)
-# def left(): bird.left(turn_speed)
-# def right(): bird.right(turn_speed)
+def forward(): bird.forward(move_speed)
+def backward(): bird.backward(move_speed)
+def left(): bird.left(turn_speed)
+def right(): bird.right(turn_speed)
 
-# screen.onkey(forward, "Up")
-# screen.onkey(backward, "Down")
-# screen.onkey(left, "Left")
-# screen.onkey(right, "Right")
-# screen.listen()
+screen.onkey(forward, "Up")
+screen.onkey(backward, "Down")
+screen.onkey(left, "Left")
+screen.onkey(right, "Right")
+screen.listen()
 
-# turtle.done()
+turtle.done()
 
