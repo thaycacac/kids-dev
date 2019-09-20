3152068ca317ad5b50c85ef0938ee148c6dffd7a
thaycacac <thaycacac@gmail.com>
2019-09-20 10:09:53 +0700

    update: func player

--- lv2/GAME/Final/main.py
+++ lv2/GAME/Final/main.py
@@ -21,13 +21,50 @@ class Pen(turtle.Turtle):
         self.penup()
         self.speed(0)
 
+#walls
+walls = []
+
+#create player
+class Player(turtle.Turtle):
+    
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("blue")
+        self.penup()
+        self.speed(0)
+        
+    def up(self):
+        move_to_x = self.xcor()
+        move_to_y = self.ycor() + 24
+        if (move_to_x, move_to_y) not in walls:
+            self.goto(move_to_x, move_to_y)
+        
+    def down(self):
+        move_to_x = self.xcor()
+        move_to_y = self.ycor() - 24
+        if (move_to_x, move_to_y) not in walls:
+            self.goto(move_to_x, move_to_y)
+        
+    def left(self):
+        move_to_x = self.xcor() - 24
+        move_to_y = self.ycor()
+        if (move_to_x, move_to_y) not in walls:
+            self.goto(move_to_x, move_to_y)
+        
+    def right(self):
+        move_to_x = self.xcor() + 24
+        move_to_y = self.ycor()
+        if (move_to_x, move_to_y) not in walls:
+            self.goto(move_to_x, move_to_y)
+
 #create levels list
 levels = []
 
 #lv1
 level_1 = [
 "XXXXXXXXXXXXXXXXXXXXXXXXX",
-"X  XXXXXXX          XXXXX",
+"XP  XXXXXXX          XXXX",
 "X  XXXXXXX  XXXXXX  XXXXX",
 "X       XX  XXXXXX  XXXXX",
 "X       XX  XXX        XX",
@@ -56,6 +93,7 @@ level_1 = [
 #add level
 levels.append(level_1)
 
+
 #setup level
 def setup_maze(level):
     for y in range(len(level)):
@@ -66,9 +104,21 @@ def setup_maze(level):
             if character == "X":
                 pen.goto(position_x, position_y)
                 pen.stamp()
+                #add coordinates to wall list
+                walls.append((position_x, position_y))
+            elif character == "P":
+                player.goto(position_x, position_y)
 
 #create instance
 pen = Pen()
+player = Player()
+
+#keyboard bindding
+turtle.listen()
+turtle.onkey(player.up, "Up")
+turtle.onkey(player.down, "Down")
+turtle.onkey(player.right, "Right")
+turtle.onkey(player.left, "Left")
 
 #set up level
 setup_maze(levels[0])
